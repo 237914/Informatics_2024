e676bf02d3311ccbea4262e8862f8172fcf0a25d
237914 <artm_petrov_2003@mail.ru>
2024-10-26 11:59:45 +0300

    a,b,x

--- golang/Lab4/Lab4.go
+++ golang/Lab4/Lab4.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func Calculate(a float64, b float64, x float64) float64 {
+func Calculate(a, b, x float64) float64 {
 	numerator := (math.Pow(math.Sqrt(math.Pow(x-a, 2)), 1.0/3.0) + math.Pow(math.Sqrt(math.Abs(x+b)), 1.0/5.0))
 	denominator := math.Pow(math.Sqrt(math.Pow(x, 2)-math.Pow(a+b, 2)), 1.0/9.0)
 	var y float64 = numerator / denominator
