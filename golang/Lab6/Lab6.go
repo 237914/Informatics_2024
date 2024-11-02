e0ddfbdf1593f39ff7c24ee4029a36f181839b9b
237914 <artm_petrov_2003@mail.ru>
2024-11-02 10:48:35 +0300

    Lab6

---
+++ golang/Lab6/Lab6.go
@@ -0,0 +1,40 @@
+package Lab6
+
+import (
+ "fmt"
+)
+
+type Pig struct {
+ Name     string
+ Age      int
+ Weight   float64
+}
+
+func NewPig(name string, age int, weight float64) *Pig {
+ return &Pig{
+  Name:   name,
+  Age:    age,
+  Weight: weight,
+ }
+}
+
+func (p *Pig) Speak() {
+ fmt.Printf("%s говорит: Хрю-хрю!\n", p.Name)
+}
+
+func (p *Pig) Eat(foodWeight float64) {
+ p.Weight += foodWeight
+ fmt.Printf("%s покушала и теперь весит %.2f кг\n", p.Name, p.Weight)
+}
+
+func (p *Pig) GrowOlder() {
+ p.Age++
+ fmt.Printf("%s теперь на год старше, ей %d года\n", p.Name, p.Age)
+}
+
+func RunLab6() {
+ myPig := NewPig("Пеппа", 2, 15.0)
+ myPig.Speak()
+ myPig.Eat(2.5)
+ myPig.GrowOlder()
+}
--- golang/main.go
+++ golang/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"isuct.ru/informatics2022/Lab4"
+	"isuct.ru/informatics2022/Lab6"
 )
 
 func main() {
 	fmt.Println("Petrov_Artem_Andreevich")
 	Lab4.RunLab4()
+	Lab6.RunLab6()
 }
