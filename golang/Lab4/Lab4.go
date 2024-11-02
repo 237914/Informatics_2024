e8ae05e03422825ef67f0335eefeaf6daca22159
237914 <artm_petrov_2003@mail.ru>
2024-11-02 09:36:18 +0300

    lab4

--- golang/Lab4/Lab4.go
+++ golang/Lab4/Lab4.go
@@ -10,7 +10,6 @@ func Calculate(a, b, x float64) float64 {
 	denominator := math.Pow(math.Sqrt(math.Pow(x, 2)-math.Pow(a+b, 2)), 1.0/9.0)
 	var y float64 = numerator / denominator
 	return y
-	// (math.Pow(math.Sqrt(math.Pow(x-a, 2)), 1.0/3.0) + math.Pow(math.Sqrt(math.Abs(x+b)), 1.0/5.0)) / math.Pow(math.Sqrt(math.Pow(x, 2)-math.Pow(a+b, 2)), 1.0/9.0)
 }
 
 func TaskA(a, b, xn, xk, deltax float64) [][]float64 {
@@ -26,8 +25,6 @@ func TaskA(a, b, xn, xk, deltax float64) [][]float64 {
 }
 
 func TaskB(a, b float64, xValue []float64) [][]float64 {
-	fmt.Println("B")
-
 	var taskBslice [][]float64
 
 	for _, x := range xValue {
@@ -47,7 +44,6 @@ func RunLab4() {
 	for _, pair := range TaskA(a, b, 1.23, 7.23, 1.2) {
 		fmt.Printf("x = %.2f\ty = %f\n", pair[0], pair[1])
 	}
-	fmt.Println()
 	fmt.Println("Задание B")
 	for _, pair := range TaskB(a, b, xValue) {
 		fmt.Printf("x = %.2f\ty = %f\n", pair[0], pair[1])
--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"isuct.ru/informatics2022/Lab4"
 )
 
 func main() {
+	fmt.Println("Petrov_Artem_Andreevich")
 	Lab4.RunLab4()
-
 }
